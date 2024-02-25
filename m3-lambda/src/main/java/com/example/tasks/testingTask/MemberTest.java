ee4e9a8532fbff82880a371a4367791464b76088
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-02-24 20:28:55 -0800

    Lambda Task

--- m3-lambda/src/main/java/com/example/tasks/testingTask/MemberTest.java
+++ m3-lambda/src/main/java/com/example/tasks/testingTask/MemberTest.java
@@ -15,6 +15,12 @@ public class MemberTest {
 
         List<Person> personList = Arrays.asList(p1,p2,p3,p4);
 
+        CheckMember checkMember = p -> p.getGender()==Gender.MALE && p.getAge() >= 18 && p.getAge() <=25;
+
+        print(personList, checkMember);
+
+        print(personList, p -> p.getGender()==Gender.MALE && p.getAge() >= 18 && p.getAge() <=25);
+
     }
 
 
