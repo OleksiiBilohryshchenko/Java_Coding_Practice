ff0e6cc9f83a2fc89861bf5fd83aa781633c536f
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-02-25 21:04:58 -0800

    comparator

---
+++ m5-comparator_interface/src/main/java/com/example/Apple.java
@@ -0,0 +1,19 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@ToString
+
+public class Apple {
+
+    private int weight;
+    private Color color;
+
+
+}
