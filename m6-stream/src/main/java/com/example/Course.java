66800b45548a9662313cec330f45b9338e919a68
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-02-26 19:04:53 -0800

    stream

---
+++ m6-stream/src/main/java/com/example/Course.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
+public class Course {
+
+    private String courseName;
+    private int courseID;
+
+
+}
---
+++ m6-stream/src/main/java/com/example/Main.java
@@ -0,0 +1,35 @@
+package com.example;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        // Creating Stream from Array
+        String[] courses = {"Java", "Spring", "Agile"};
+
+        Stream<String> courseStream = Arrays.stream(courses);
+
+        // Creating Stream from Collection
+
+        List<String> courseList = Arrays.asList("Java", "Spring", "Agile");
+
+        Stream <String> courseStream2 = courseList.stream();
+
+        List<Course> myCourses = Arrays.asList(
+                new Course("Java",100),
+                new Course("Spring",150),
+                new Course("Agile",200)
+        );
+
+        Stream<Course> myCourseStream = myCourses.stream();
+
+
+
+
+    }
+
+}
