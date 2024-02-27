25388b34167087ed057d596f9e9b773b91a218db
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-02-26 20:02:49 -0800

    stream

--- m6-stream/src/main/java/com/example/StreamOperations.java
+++ m6-stream/src/main/java/com/example/StreamOperations.java
@@ -1,12 +1,27 @@
 package com.example;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
 public class StreamOperations {
 
     public static void main(String[] args) {
 
+        List<Integer> list = Arrays.asList(1,2,2,3,4);
+//        list.forEach(System.out::println);
 
+        // Intermediate operations
 
+        // Filter
+        System.out.println("Filter");
+        list.stream().filter(i -> i%2 == 0).forEach(System.out::println);
 
+        // Distinct
+        System.out.println("Distinct");
+        Stream<Integer> str= list.stream().filter(i->i%2==0).distinct();
+        str.forEach(System.out::println);
 
     }
 
