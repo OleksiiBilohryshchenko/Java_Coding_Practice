e744d65a55dc4879cc03c144555da3d31a80f3d3
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-03 19:42:01 -0600

    finding match

---
+++ m6-stream/src/main/java/com/example/Reducing.java
@@ -0,0 +1,44 @@
+package com.example;
+
+import com.example.task.Dish;
+import com.example.task.DishData;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+public class Reducing {
+
+    public static void main(String[] args) {
+
+
+        List<Integer> numbers = Arrays.asList(4,5,3,9);
+//        int sum = 0;
+//        for (Integer number : numbers) {
+//            sum = sum + number;
+//        }
+
+        //int result = numbers.stream().reduce(0,(a,b) -> a+b);
+        int result = numbers.stream().reduce(0, Integer::sum);
+        System.out.println(result);
+
+        Optional<Integer> result2 = numbers.stream().reduce((a, b) -> a+b);
+        System.out.println(result2.get());
+
+        System.out.println("Dish calories total");
+        //Optional<Integer> calTotal = ...
+        int calTotal = DishData.getAll().stream().map(Dish::getCalories).reduce((a,b) -> a+b).get();
+        System.out.println(calTotal);
+
+        // Max and Min
+        Optional<Integer> min = numbers.stream().reduce(Integer::min);
+        Optional<Integer> max = numbers.stream().reduce(Integer::max);
+        Optional<Integer> sum = numbers.stream().reduce(Integer::sum);
+        System.out.println(min.get());
+        System.out.println(max.get());
+        System.out.println(sum.get());
+
+    }
+
+
+}
