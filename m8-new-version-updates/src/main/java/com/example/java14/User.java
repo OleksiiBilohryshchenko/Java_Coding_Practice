5b11f99238bfa1f70859bc94defeb2fec343bb52
OleksiiBilohryshchenko <obelogrishchenko@gmail.com>
2024-03-07 22:14:01 -0800

    java14

---
+++ m8-new-version-updates/src/main/java/com/example/java14/User.java
@@ -0,0 +1,14 @@
+package com.example.java14;
+
+public record User(String name, String address) {
+
+
+    // immutable - private final
+
+    // All args const
+    // getters
+    // to string
+    // equals
+    // hash code
+
+}
---
+++ m8-new-version-updates/src/main/java/com/example/java14/UserTest.java
@@ -0,0 +1,12 @@
+package com.example.java14;
+
+public class UserTest {
+    public static void main(String[] args) {
+
+
+        User user = new User("Alex", "IL");
+        System.out.println(user.name());
+        System.out.println(user.toString());
+
+    }
+}
